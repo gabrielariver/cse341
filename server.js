fbf3a7f4d9dbbc59bcbe9c13f2b3775f53b4ebec
gabrielariver <riv23011@byui.edu>
2025-01-11 12:00:30 -0500

    API routes are working - Update lesson 1

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongodb = require('./data/contacts'); 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {console.log(`The database is running on port ${port}`)});
+    }
+}); 
